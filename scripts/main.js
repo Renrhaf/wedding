c96468f0c824bec40a238676c04494bdf25eb7c2
Quentin Fahrner <fahrner.quentin@gmail.com>
2023-11-27 23:54:08 +0100

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,145 @@
+// Add your javascript here
+// Don't forget to add it into respective layouts where this js file is needed
+$(document).ready(function() {
+  AOS.init({
+    // uncomment below for on-scroll animations to played only once
+    // once: true
+  }); // initialize animate on scroll library
+});
+
+// Smooth scroll for links with hashes
+$("a.smooth-scroll").click(function(event) {
+  // On-page links
+  if (
+    location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") &&
+    location.hostname == this.hostname
+  ) {
+    // Figure out element to scroll to
+    var target = $(this.hash);
+    target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+    // Does a scroll target exist?
+    if (target.length) {
+      // Only prevent default if animation is actually gonna happen
+      event.preventDefault();
+      $("html, body").animate(
+        {
+          scrollTop: target.offset().top
+        },
+        1000,
+        function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) {
+            // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          }
+        }
+      );
+    }
+  }
+});
+
+// Photo Filter
+var activeFilter = "all";
+
+$(".ww-filter-button").on("click", function(e) {
+  // remove btn-primary from all buttons first
+  $(".ww-filter-button").removeClass("btn-primary");
+  $(".ww-filter-button").addClass("btn-outline-primary");
+
+  // add btn-primary to active button
+  var button = $(this);
+  button.removeClass("btn-outline-primary");
+  button.addClass("btn-primary");
+  filterItems(button.data("filter"));
+  e.preventDefault();
+});
+
+function filterItems(filter) {
+  if (filter === activeFilter) {
+    return;
+  }
+
+  activeFilter = filter;
+  $(".ww-gallery .card").each(function() {
+    var card = $(this);
+    var groups = card.data("groups");
+    var show = false;
+    if (filter === "all") {
+      show = true;
+    } else {
+      for (var i = 0; i < groups.length; i++) {
+        if (groups[i] === filter) {
+          show = true;
+        }
+      }
+    }
+    // hide everything first
+    card.fadeOut(400);
+    setTimeout(function() {
+      if (show && !card.is(":visible")) {
+        card.fadeIn(400);
+      }
+    }, 500);
+  });
+}
+
+// Light Box
+$(document).on("click", '[data-toggle="lightbox"]', function(event) {
+  event.preventDefault();
+  $(this).ekkoLightbox();
+});
+
+// Airtable connexion.
+$(document).ready(function() {
+  var Airtable = require('airtable');
+
+  Airtable.configure({
+    endpointUrl: 'https://api.airtable.com',
+    apiKey: 'pattCXU1Q0DciomLs.6b5c9eae36f321b4b19e85b98876e4b049cdf6836e50be8e1435f2899045b045'
+  });
+
+  var base = Airtable.base('appJshxpUksuHpFEq');
+  base('Mariage').select({
+    maxRecords: 500,
+    view: "Vue de tableur"
+  }).eachPage(function page(records, fetchNextPage) {
+    // This function (`page`) will get called for each page of records.
+
+    records.forEach(function(record) {
+      console.log('Retrieved', record.get('Nom'));
+    });
+
+    // To fetch the next page of records, call `fetchNextPage`.
+    // If there are more records, `page` will get called again.
+    // If there are no more records, `done` will get called.
+    fetchNextPage();
+
+  }, function done(err) {
+    if (err) { console.error(err); return; }
+  });
+
+  // @see https://airtable.com/appJshxpUksuHpFEq/api/docs#javascript/table:mariage:create
+  /*base('Mariage').create([
+    {
+      "fields": {
+        "Nom": "Mihika Mahakal",
+        "Email": "mihi7ka@gmail.com",
+        "Présence": true
+      }
+    }
+  ], function(err, records) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    records.forEach(function (record) {
+      console.log(record.getId());
+    });
+  });*/
+})
